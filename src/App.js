b8578f3e623fa3fa0fec10061164ffa6f6e6d4cb
Arsalan Ali <arsalanali.arain1@gmail.com>
2021-08-25 22:07:51 +0500

    removed funtional component section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
@@ -14,15 +13,9 @@ class App extends Component {
     showPersons: false
   }
 
-  // switchNameHandler = () => { --------------- passed argument
   switchNameHandler = (newName) => {
-    // DON'T DO THIS: this.state.persons[0].name = "maximilian";
-
-    // ye sirf person ko hi change kare ga baqi jaise yahan ek otherState hai us ko or baqi jo bhi 
-    // hongi un ko merge ker de ga
     this.setState({
       persons: [
-        // { name: 'maximilian', age: 28 },
         { name: newName, age: 28 },
         { name: 'manu', age: 23 },
         { name: 'john', age: 27 }
@@ -88,56 +81,5 @@ class App extends Component {
     );
   }
 }
-
-
-// // FUNCTIONAL COMPONENTS --> manipulate state of functional component using useState Hook...
-// // useState() hook allows to manage state in the functional component
-// const App = props => {
-
-//   // useState returns an array of exactly 2 elements 
-//   // store returned values using array distructing
-//   const [personsState, setPersonsState] = useState({ // pass initial state
-//     // personsState give access to this state means old state
-//     persons: [
-//       { name: 'max', age: 28 },
-//       { name: 'manu', age: 23 },
-//       { name: 'john', age: 26 }
-//     ],
-//     // otherState: 'some other value'
-//     // is ko is liye comment ker diya q k ab is ki zaroorat nahi is k liye ek or useState hai
-//   });
-
-//   // using multiple useState to prevent over-write state problem
-//   const [otherState, setOtherState] = useState('some other value');
-
-//   console.log(personsState, otherState);
-
-//   const switchNameHandler = () => {
-//     // jab hooks use kerte hain to new state old state ko over-write ker deti hai na k merge unchanged 
-//     // content se. is problems k 2 sol hain ek manually data add ker dain old state se or ek useState 
-//     // ko multiple times use karain ye recommended method hai useState wala
-//     setPersonsState({
-//       persons: [
-//         { name: 'maximilian', age: 28 },
-//         { name: 'manu', age: 23 },
-//         { name: 'john', age: 27 }
-//       ],
-//       // otherState: personsState.otherState
-//     })
-//   }
-
-//   return (
-//     <div className="App">
-//       <h1>Hi, I'm React App</h1>
-//       <p>This is really working!</p>
-
-//       <button onClick={switchNameHandler}>Switch Name</button>
-//       <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-//       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobbies: Racing</Person>
-//       <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
-//     </div>
-//   );
-// }
-
 
 export default App;
